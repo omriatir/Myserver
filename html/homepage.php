2c352da1a39c6b4eb792ab4eefeaf539769eec53
omriatir <88487088+omriatir@users.noreply.github.com>
2024-07-02 11:33:24 +0300

    Update homepage.php

--- html/homepage.php
+++ html/homepage.php
@@ -17,7 +17,7 @@
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email" required/>
         </div>
         <div class="form-group">
             <label for="message">Message:</label>
