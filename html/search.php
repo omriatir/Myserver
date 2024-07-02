d042406d3c5c14fbd2cf06bbe6575cee3c8f96c8
omriatir <88487088+omriatir@users.noreply.github.com>
2024-07-02 10:13:27 +0300

    Update search.php

--- html/search.php
+++ html/search.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic Search Page</title>
-    <link rel="stylesheet" href="styles1.css">
+    <link rel="stylesheet" href="styles_sp.css">
 </head>
 <body>
     <div class="container">
